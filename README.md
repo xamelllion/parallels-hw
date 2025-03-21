ee0cfeca5603a241923e8c350b8af8d280560760
Viacheslav Sidorov <me@xamelllion.ru>
2025-03-21 20:04:16 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Parallels hw
+
+## Конфигурация
+```
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=clang -GNinja -Bbuild
+```
+
+## Сборка
+```
+cmake --build build
+```
+
+## Форматирование
+```
+./scripts/format.sh
+```
---
+++ scripts/ci-style-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SRC_DIR="src"
+
+if [ ! -d "$SRC_DIR" ]; then
+    echo "Error: Directory '$SRC_DIR' not found."
+    exit 1
+fi
+
+files=$(find "$SRC_DIR" -type f \( -name "*.c" -o -name "*.h" \))
+clang-format --Werror --dry-run -i $files
