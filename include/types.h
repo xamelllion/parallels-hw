a57c5598e36d389601b3121345a03b6b3f480c83
Viacheslav Sidorov <me@xamelllion.ru>
2025-03-21 22:53:06 +0300

    feat: use stb to work with images

--- include/types.h
+++ include/types.h
@@ -1,27 +1,5 @@
 typedef struct {
-    short signature;
-    int fileSize;
-    int reserved;
-    int dataOffset;
-} header_struct;
-
-typedef struct {
-    int size;
-    int width;
-    int height;
-    short planes;
-    short bitcount;
-    int compression;
-    int imageSize;
-    int XpixelsPerM;
-    int YpixelsPerM;
-    int colorsUsed;
-    int colorsImportant;
-} info_header;
-
-typedef struct {
-    char b;
-    char g;
     char r;
-    char a;
+    char g;
+    char b;
 } pixel_struct;
