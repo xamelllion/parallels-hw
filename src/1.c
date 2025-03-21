cc4a9706235ddd7c21f3e118384f4d6ca30693ac
Viacheslav Sidorov <me@xamelllion.ru>
2025-03-21 21:21:22 +0300

    feat: read & write files

--- src/1.c
+++ src/1.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <utils.h>
 
 int main() {
-    printf("Hello world\n");
+    file_info image_file = read_file("examples/image.bmp");
+    write_file("examples/image2.bmp", image_file);
+
+    free(image_file.file);
 }
