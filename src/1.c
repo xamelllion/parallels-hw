5b10f7fbd9df7bb4c57901ef1a0c854c15f26735
Viacheslav Sidorov <me@xamelllion.ru>
2025-03-21 22:01:21 +0300

    feat: image inversion

--- src/1.c
+++ src/1.c
@@ -1,10 +1,39 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <types.h>
 #include <utils.h>
+
+void inverse_image(file_info file_info) {
+    header_struct hs;
+    info_header ih;
+
+    memcpy(&hs, file_info.file, 14);
+    memcpy(&ih, file_info.file + 14, 40);
+
+    printf("size : %d\n", ih.size);
+    printf("width : %d\n", ih.width);
+    printf("height : %d\n", ih.height);
+    printf("bitcount : %d\n", ih.bitcount);
+    printf("compression : %d\n", ih.compression);
+    printf("imageSize : %d\n", ih.imageSize);
+    printf("colorsUsed : %d\n", ih.colorsUsed);
+    printf("colorsImportant : %d\n", ih.colorsImportant);
+
+    char* start = file_info.file + 14 + ih.size;
+    for (ssize_t i = 0; i < ih.imageSize / 4; i++) {
+        pixel_struct* pixel = (pixel_struct*)(&start[i * 4]);
+        pixel->r = 255 - pixel->r;
+        pixel->g = 255 - pixel->g;
+        pixel->b = 255 - pixel->b;
+    }
+}
 
 int main() {
     file_info image_file = read_file("examples/image.bmp");
-    write_file("examples/image2.bmp", image_file);
+    inverse_image(image_file);
+    write_file("examples/result.bmp", image_file);
 
     free(image_file.file);
 }
